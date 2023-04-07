04659f61d185fe7c2b11d5d9b3d5551250456556
MotaharSumit <sumitbii91@gmail.com>
2023-04-07 14:58:33 +0600

    abcd

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Tshirt from '../Tshirt/Tshirt';
+import Cart from '../Cart/Cart';
+import './Home.css'
 
 const Home = () => {
     const tshirts = useLoaderData();  
-    return (
-        <div>
-            <h2>This is Home: {tshirts.length}</h2>
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = tshirt => {
+        console.log(thsirt);
+    }
+    
+        <div className='home-container'>
+            <div className='tshirts-container'>
+            {
+                tshirts.map(tshirt => <Tshirt
+                key={tshirt._id}
+                tshirt={tshirt}
+                handleAddToCart={handleAddToCart}
+                ></Tshirt>)
+            }
+            </div>
+            <div className='cart-container'> 
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
